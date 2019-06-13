f6ed97b493b37ddfc934b48febaa4b30ccb1f874
saurabhse <saurabh10seth@gmail.com>
2019-06-13 17:03:08 +0200

    Update jira_alerts.py

--- jira-desktop-notification/jira_alerts.py
+++ jira-desktop-notification/jira_alerts.py
@@ -1,27 +1,21 @@
-import time
-list_a = [1,2,3]
-list_b = [2,3]
-second = set(list_b)
-l = [item for item in list_a if item not in list_b]
-
-l1 = [a for a in list_a+list_b if (a not in list_a) or (a not in list_b)]
+from jira import JIRA
 from win32api import *
 from win32gui import *
 import win32con
 import sys, os
-import struct
+import json
 import time
+import datetime
 
-class WindowsBalloonTip:
+class WindowsNotification:
     def __init__(self, title, msg):
         message_map = {
                 win32con.WM_DESTROY: self.OnDestroy,
         }
-        # Register the Window class.
         wc = WNDCLASS()
         hinst = wc.hInstance = GetModuleHandle(None)
         wc.lpszClassName = "PythonTaskbar"
-        wc.lpfnWndProc = message_map # could also specify a wndproc.
+        wc.lpfnWndProc = message_map
         classAtom = RegisterClass(wc)
         # Create the Window.
         style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
@@ -32,51 +26,62 @@ class WindowsBalloonTip:
         iconPathName = os.path.abspath(os.path.join( sys.path[0], "balloontip.ico" ))
         icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
         try:
-           hicon = LoadImage(hinst, iconPathName,
+           self.hicon = LoadImage(hinst, iconPathName,
                     win32con.IMAGE_ICON, 0, 0, icon_flags)
         except:
-          hicon = LoadIcon(0, win32con.IDI_APPLICATION)
+          self.hicon = LoadIcon(0, win32con.IDI_APPLICATION)
         flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
-        nid = (self.hwnd, 0, flags, win32con.WM_USER+20, hicon, "tooltip")
+        nid = (self.hwnd, 0, flags, win32con.WM_USER+20, self.hicon, "tooltip")
         Shell_NotifyIcon(NIM_ADD, nid)
-        Shell_NotifyIcon(NIM_MODIFY,
-                         (self.hwnd, 0, NIF_INFO, win32con.WM_USER+20,
-                          hicon, "Balloon  tooltip",title,200,msg))
-        # self.show_balloon(title, msg)
-        time.sleep(10)
-        DestroyWindow(self.hwnd)
+
     def OnDestroy(self, hwnd, msg, wparam, lparam):
         nid = (self.hwnd, 0)
         Shell_NotifyIcon(NIM_DELETE, nid)
         PostQuitMessage(0) # Terminate the app.
-def balloon_tip(title, msg):
-    w=WindowsBalloonTip(msg, title)
 
+    def showNotification(self,title,msg):
+        Shell_NotifyIcon(NIM_MODIFY,
+                         (self.hwnd, 0, NIF_INFO, win32con.WM_USER + 20,
+                          self.hicon, "Balloon  tooltip", title, 200, msg))
+        time.sleep(10)
+        #DestroyWindow(self.hwnd)
+
+def notification_window(title, msg):
+    w=WindowsNotification(msg, title)
+    return w
+
+def notify(title, msg,obj):
+    obj.showNotification(title, msg)
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+options = {'server': 'https://jira.kindredgroup.com'}
+jira = JIRA(options, basic_auth=(config['JIRA']['USER_NAME'], config['JIRA']['PASSWORD']))
 ticket_list = []
-while True:
-    with open('C:\Saurabh\jira\sample.txt') as f:
-        issues = f.read().splitlines()
+obj = notification_window('hello', 'world')
+now = datetime.datetime.now()
+today6pm = now.replace(hour=18, minute=0, second=0, microsecond=0)
 
-    index = 0
+while now < today6pm if config['PARAMS']['SHUTDOWN'] else True:
+    issue_list = jira.search_issues('filter=19578')
+    issues=[]
+    new_tickets=[]
+    for issue in issue_list:
+        issues.append(issue.key)
     removed_list = [a for a in ticket_list if a not in issues]
-    print(removed_list)
-    print(ticket_list)
-    print(issues)
+    #print(removed_list)
+    #print(ticket_list)
+    #print(issues)
     for removed in removed_list:
         ticket_list.remove(removed)
     for issue in issues:
-        index = index + 1
         if str(issue) not in ticket_list:
-            print(str(index) + ' new ticket arrived', str(issue))
+            print('New ticket arrived => ', str(issue))
             ticket_list.append(str(issue))
-    time.sleep(20)
-    balloon_tip('hello', 'world')
-    
-    from jira import JIRA
-import time
-from pprint import pprint
-options = {'server': ''}
-jira = JIRA(options, basic_auth=('', ''))
-ticket_list = []
-while True:
-    issues = jira.search_issues('filter=')
+            new_tickets.append(str(issue))
+
+    if len(new_tickets) > 0:
+        notify(",".join(new_tickets), 'New Incident Assigned', obj)
+    time.sleep(config['PARAMS']['REFRESH_INTERVAL'])
+
+
